0c387d0aa047a7a71965725cf9ad534e7546b3ab
WooHyeong Choi <48896148+dn7638@users.noreply.github.com>
2024-10-18 20:58:11 +0900

    [level 3] Title: 대장균의 크기에 따라 분류하기 2, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
+++ 프로그래머스/3/301649. 대장균의 크기에 따라 분류하기 2/대장균의 크기에 따라 분류하기 2.sql
@@ -1,18 +1,14 @@
-SELECT ID, COLONY_NAME
-FROM(
-    SELECT ID,
-        CASE
-            WHEN (row_num-1) DIV (total DIV 4) = 0 THEN 'CRITICAL'
-            WHEN (row_num-1) DIV (total DIV 4) = 1 THEN 'HIGH'
-            WHEN (row_num-1) DIV (total DIV 4) = 2 THEN 'MEDIUM'
-            WHEN (row_num-1) DIV (total DIV 4) = 3 THEN 'LOW'
-            ELSE 'null'
+WITH SUB_TABLE AS (
+    SELECT ID, NTILE(4) over (ORDER BY SIZE_OF_COLONY DESC) as TYPE
+    FROM ECOLI_DATA
+    )
+SELECT ID, 
+    CASE
+        WHEN TYPE = 1 THEN 'CRITICAL'
+        WHEN TYPE = 2 THEN 'HIGH'
+        WHEN TYPE = 3 THEN 'MEDIUM'
+        WHEN TYPE = 4 THEN 'LOW'
         END AS COLONY_NAME
-    FROM (
-        SELECT ROW_NUMBER() OVER (ORDER BY SIZE_OF_COLONY DESC) AS row_num, ID, SIZE_OF_COLONY,
-               (SELECT COUNT(*) FROM ECOLI_DATA) AS total
-        FROM ECOLI_DATA
-        ) AS subquery
-    ) AS final
-ORDER BY ID ASC;
-
+FROM SUB_TABLE
+ORDER BY ID ASC
+    
