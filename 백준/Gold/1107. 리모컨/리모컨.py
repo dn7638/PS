4761f1696b3fda6b889e2ab72e3595b198a9d4f8
WooHyeong Choi <48896148+dn7638@users.noreply.github.com>
2024-10-20 17:34:28 +0900

    [Gold V] Title: 리모컨, Time: 2132 ms, Memory: 34200 KB -BaekjoonHub

--- 백준/Gold/1107. 리모컨/리모컨.py
+++ 백준/Gold/1107. 리모컨/리모컨.py
@@ -8,31 +8,30 @@ result = 500000
 if M > 0:
     buttons = set(list(map(int, input().split(' '))))
     stack = deque()
-    stack.append([])
+    stack.append((0,0))
 
     while stack:
-        cur_list = stack.pop()
+        num, cnt = stack.pop()
 
-        if len(cur_list) == 0:
+
+        if cnt == 0:
             number = 100
         else:
-            number = int(''.join(list(map(str, cur_list))))
+            number = num
 
         # |만들어진 숫자 - 목표| + 숫자를 누른 횟수
-        temp = min(abs(dst_int - number) + len(cur_list), abs(dst_int - 100))
+        temp = min(abs(dst_int - number) + cnt, abs(dst_int - 100))
 
         if result > temp:
             result = temp
 
-        if len(cur_list) == len(dst_str) + 1:
+        if cnt == len(dst_str) + 1:
             continue
 
         for i in range(10):
             if i in buttons:
                 continue
-            next_list = cur_list[:]
-            next_list.append(i)
-            stack.append(next_list)
+            stack.append((num * 10 + i, cnt + 1))
 
     print(result)
 
