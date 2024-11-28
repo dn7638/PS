13426b061e46d14c63a87741d6c5bb9509a49bb2
WooHyeong Choi <48896148+dn7638@users.noreply.github.com>
2024-11-28 18:18:34 +0900

    [Platinum IV] Title: K번째 최단경로 찾기, Time: 7616 ms, Memory: 75420 KB -BaekjoonHub

--- 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
+++ 백준/Platinum/1854. K번째 최단경로 찾기/K번째 최단경로 찾기.py
@@ -1,5 +1,4 @@
 import heapq
-from collections import deque
 
 n, m, k = map(int, input().split(' '))
 nodes = [[] for _ in range(n + 1)]
@@ -8,15 +7,13 @@ for i in range(m):
     a, b, time = map(int, input().split(' '))
     nodes[a].append((time, b))
 
-pass_list = [[] for _ in range(n+1)]
-# 방문 가능? 불가능?
+pass_list = [[] for _ in range(n + 1)]
 
-# 방문 불가능이더라도 -> 1번째면?
+
 def dijkstra(k):
-
     queue = []
     heapq.heappush(queue, (0, 1))
-    heapq.heappush(pass_list[1],[0,0])
+    heapq.heappush(pass_list[1], [0, 0])
 
     while queue:
 
@@ -26,18 +23,17 @@ def dijkstra(k):
             if len(pass_list[next]) < k:
                 heapq.heappush(queue, (cur_time + time, next))
                 heapq.heappush(pass_list[next], [-(cur_time + time), cur_time + time])
+
+            # 노드, 간선 정보에 따라 항상 pass_list[next]에 최소 순서로 들어간다는 보장이 없음...! 그래서 힙사용
             elif pass_list[next][0][1] > cur_time + time:
-                heapq.heappush(queue, (cur_time+time, next))
+                heapq.heappush(queue, (cur_time + time, next))
                 heapq.heappop(pass_list[next])
-                heapq.heappush(pass_list[next], [-(cur_time+time), cur_time+time])
+                heapq.heappush(pass_list[next], [-(cur_time + time), cur_time + time])
 
 
 dijkstra(k)
 
-# for i in pass_list:
-#     print(i)
-
-for i in range(1,n+1):
+for i in range(1, n + 1):
     if len(pass_list[i]) < k:
         print(-1)
     else:
